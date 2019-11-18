06d211769af95b0f90490eb0f07d4607bee858d4
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-11-17 18:08:07 -0600

    Create assert_equal function

--- conway_test.sh
+++ conway_test.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
+assert_equal() {
+  if [ "$1" = "$2" ]; then
+    echo "pass"
+  else
+    echo "failure: got $1 instead of expected $2"
+  fi
+}
+
 
 echo "A 1-by-1 array with a dead cell is returned unchanged"
 
 INPUT=(0)
-EXPECTED=(0)
-OUTPUT=$(./conway.sh $INPUT)
+EXPECTED_OUTPUT=(0)
+ACTUAL_OUTPUT=$(./conway.sh $INPUT)
 
-if [ "$OUTPUT" = "$EXPECTED" ]; then
-  echo "pass";
-else
-  echo "failure: expected $EXPECTED, but got $OUTPUT"
-fi
+assert_equal $ACTUAL_OUTPUT $EXPECTED_OUTPUT
 
 
 echo "A 2-by-1 array with all dead cells is returned unchanged"
 
 INPUT=(0, 0)
-EXPECTED=(0, 0)
-OUTPUT=$(./conway.sh $INPUT)
+EXPECTED_OUTPUT=(0, 0)
+ACTUAL_OUTPUT=$(./conway.sh $INPUT)
 
-if [ "$OUTPUT" = "$EXPECTED" ]; then
-  echo "pass";
-else
-  echo "failure: expected $EXPECTED, but got $OUTPUT"
-fi
+assert_equal $ACTUAL_OUTPUT $EXPECTED_OUTPUT
