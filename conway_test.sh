c8c96dc4db4ded3096a2af8b19b108ac017d075d
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-11-17 18:12:32 -0600

    Move all tests to a function and use local variables

--- conway_test.sh
+++ conway_test.sh
@@ -8,20 +8,23 @@ assert_equal() {
   fi
 }
 
+conway_tests(){
+  echo "A 1-by-1 array with a dead cell is returned unchanged"
 
-echo "A 1-by-1 array with a dead cell is returned unchanged"
+  local INPUT=(0)
+  local EXPECTED_OUTPUT=(0)
+  local ACTUAL_OUTPUT=$(./conway.sh $INPUT)
 
-INPUT=(0)
-EXPECTED_OUTPUT=(0)
-ACTUAL_OUTPUT=$(./conway.sh $INPUT)
-
-assert_equal $ACTUAL_OUTPUT $EXPECTED_OUTPUT
+  assert_equal $ACTUAL_OUTPUT $EXPECTED_OUTPUT
 
 
-echo "A 2-by-1 array with all dead cells is returned unchanged"
+  echo "A 2-by-1 array with all dead cells is returned unchanged"
 
-INPUT=(0, 0)
-EXPECTED_OUTPUT=(0, 0)
-ACTUAL_OUTPUT=$(./conway.sh $INPUT)
+  INPUT=(0, 0)
+  EXPECTED_OUTPUT=(0, 0)
+  ACTUAL_OUTPUT=$(./conway.sh $INPUT)
 
-assert_equal $ACTUAL_OUTPUT $EXPECTED_OUTPUT
+  assert_equal $ACTUAL_OUTPUT $EXPECTED_OUTPUT
+}
+
+conway_tests
